b94523d70d306abc32d593a9981559198afdbecb
Dollypee48 <akinolad22@gmail.com>
2025-07-13 19:55:21 +0100

    updated the protectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,14 @@
+// src/components/ProtectedRoute.jsx
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
+
   const { user } = useSelector((state) => state.auth);
 
-  if (!user) {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  if (!user && !storedUser) {
     return <Navigate to="/login" replace />;
   }
 
